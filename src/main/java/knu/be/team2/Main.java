d02ed1264f6d5a8fe394cf4bfda1efbf71b11ff8
anacnoda77 <sm1108shin@naver.com>
2024-06-04 20:10:10 +0900

    feat: 나머지feat: 나머지feat: 나머지

--- src/main/java/knu/be/team2/Main.java
+++ src/main/java/knu/be/team2/Main.java
@@ -4,5 +4,12 @@ package knu.be.team2;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        for(int i = 1; i<=15; i++) {
+
+            else {
+                System.out.println(i);
+            }
+        }
     }
 }
