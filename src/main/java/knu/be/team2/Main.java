0a87381c609051685a3743829aa781bfa0805e4e
xkrwjdals <developjmt@gmail.com>
2024-06-04 20:15:24 +0900

    feat : Create three_and_five method
    Print fizzbuzz when integer n is common multiple of 3 and 5
    Otherwise, print integer n

--- src/main/java/knu/be/team2/Main.java
+++ src/main/java/knu/be/team2/Main.java
@@ -4,5 +4,11 @@ package knu.be.team2;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+    }
+    static String three_and_five(int n) {
+        if ((n % 3 == 0) && (n % 5 == 0))
+            return "fizzbuzz";
+        return Integer.toString(n);
     }
 }
